c952c72678886fd425c21dee1ee39e36bd786c3d
rubyazz <ersultan.abduvalov@gmail.com>
2024-11-20 22:23:41 +0500

    [setup project]

---
+++ backend/points/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class PointOfInterest(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    category = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
